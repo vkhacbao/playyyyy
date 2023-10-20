171a352e715ccea5dbe8bb31b9511b6cd3538b6f
vkhacbao2000 <vkhacbao2000@gmail.com>
2023-10-20 11:14:44 +0700

    Initial commit

---
+++ src/main/java/com/example/playyyyy/PlayyyyyApplication.java
@@ -0,0 +1,13 @@
+package com.example.playyyyy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayyyyyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayyyyyApplication.class, args);
+    }
+
+}
